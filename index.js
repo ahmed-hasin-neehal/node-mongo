efe105ac68a3500f701f9be3ca1e6c3c2051e4a4
ahmed-hasin-neehal <60386953+ahmed-hasin-neehal@users.noreply.github.com>
2020-10-03 12:29:05 +0600

    get, post done

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
 
 const app = express();
 
+app.use(cors());
+
+app.use(bodyParser.json());
+
+const users = ['Asad', 'Moin', 'Sazzad', 'Naeem', 'Ahmed','Hossain'];
+
+//get
 app.get('/', (req, res) => {
-    res.send('Thank you for calling me');
+    const fruit = {
+        product: 'mango',
+        price: 220
+    }
+    res.send(fruit);
+})
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    //console.log(req.query.sort);
+    const name = users[id];
+    res.send({id, name});
+})
+
+// post
+app.post('/addUser', (req, res) => {
+    //save to database
+    const user = req.body;
+    user.id = 45;
+    res.send(user);
 })
 
 app.listen(3000, () => console.log('Listening to port 3000'));
